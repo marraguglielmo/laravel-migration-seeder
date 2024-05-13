1884bd23de975297f63d7b803b0708e4c16628ff
Guglielmo Marra <156308467+marraguglielmo@users.noreply.github.com>
2024-05-13 16:15:02 +0200

    [add] migration

---
+++ database/migrations/2024_05_13_141045_create_trains_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('trains', function (Blueprint $table) {
+            $table->id();
+            $table->string('company', 40);
+            $table->string('slug', 40)->unique();
+            $table->string('departure_station', 30);
+            $table->string('arrival_station', 30);
+            $table->dateTime('departure_time');
+            $table->dateTime('arrival_time');
+            $table->string('code', 10);
+            $table->tinyInteger('coaches_number')->unsigned();
+            $table->boolean('is_on_time')->default(true);
+            $table->boolean('is_cancelled')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('trains');
+    }
+};
