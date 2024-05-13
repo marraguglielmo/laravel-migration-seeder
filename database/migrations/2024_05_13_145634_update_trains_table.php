dc99104e08defda3def98d2c4b1cbdb3a2ea2d0f
Guglielmo Marra <156308467+marraguglielmo@users.noreply.github.com>
2024-05-13 17:05:49 +0200

    [add] migration update

---
+++ database/migrations/2024_05_13_145634_update_trains_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('trains', function (Blueprint $table) {
+            $table->string('conductor', 20)->after('slug');
+            $table->decimal('ticket_price', 5, 2)->after('arrival_time');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('trains', function (Blueprint $table) {
+            $table->dropColumn('conductor');
+            $table->dropColumn('ticket_price');
+        });
+    }
+};
