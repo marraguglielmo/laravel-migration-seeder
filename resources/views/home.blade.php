d90980ad70a3df02dbbf7be339bc456405d3488d
marraguglielmo <gugmarra@gmail.com>
2024-05-14 21:52:29 +0200

    [add] scss

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,6 +6,12 @@
 
 @section('content')
 
-<h1>Home</h1>
+<h1 class="text-center">Home</h1>
+
+<div class="container">
+    <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat accusamus animi officia rerum. Ullam, quod reprehenderit ratione corrupti odit consectetur harum et, cumque tempora eligendi iste quaerat illum. Aspernatur a fuga optio itaque ullam dignissimos ipsa illo veniam? Distinctio iure repellendus accusamus excepturi aliquid, eligendi quo a repellat unde quasi vitae, ipsa molestiae laborum qui maiores sunt quod. Ad ullam adipisci non dicta voluptatem perspiciatis, quam iusto beatae ea fugiat. Magni minus eligendi saepe sapiente architecto adipisci sed! Voluptate, obcaecati deleniti assumenda voluptatibus molestiae quibusdam. Laudantium neque adipisci laboriosam exercitationem repudiandae reprehenderit minima excepturi quaerat ea quibusdam, libero eos optio, tenetur maxime nobis tempore perspiciatis eveniet animi facere autem non. Nulla corporis aut, officia veritatis, voluptas blanditiis minima adipisci ipsa, commodi tenetur nisi laborum quo fuga iste distinctio beatae cum accusantium dolores neque corrupti impedit earum doloremque porro laudantium. Accusantium at dolore aliquid debitis, omnis atque quaerat saepe sapiente facere error! Animi cupiditate repudiandae atque ut delectus assumenda eum accusantium sint aut nemo facilis nulla ipsam aliquid molestiae iusto, hic inventore a illum voluptas! Eaque exercitationem cumque iusto repudiandae omnis incidunt harum laboriosam amet reprehenderit, temporibus culpa voluptatum quaerat, quod quae. Expedita rerum molestiae quae necessitatibus, aperiam deserunt non praesentium?
+    </p>
+</div>
 
 @endsection
--- resources/views/tickets.blade.php
+++ resources/views/tickets.blade.php
@@ -6,16 +6,42 @@
 
 @section('content')
 
-<h1>Tickets</h1>
+<h1 class="text-center">Tickets</h1>
 
 <div class="container">
     <div class="row row-cols-2">
         @foreach ($trains as $train)
         <div class="col px-2 py-3">
             <div class="ticket-card border border-2">
-                <div class="cities">
-                    <span class="d_station">{{$train->departure_station}}</span>
-                    <span class="a_station">{{$train->arrival_station}}</span>
+                {{-- card top --}}
+                <div class="card-top d-flex justify-content-between  ">
+                    <div>
+                        <div class="company fs-3">
+                            {{$train->company}}
+                        </div>
+                        {{-- cities --}}
+                        <div class="cities">
+                            <span class="d_station">{{$train->departure_station}}</span>
+                            <span class="a_station">{{$train->arrival_station}}</span>
+                        </div>
+                    </div>
+                    {{-- time --}}
+                    <div>
+                        <div class="d-flex flex-column justify-content-between text-end">
+                            <span>Partenza: {{$train->departure_time}}</span>
+                            <span>Arrivo: {{$train->arrival_time}}</span>
+                        </div>
+                    </div>
+
+                </div>
+                {{-- line --}}
+                <div class="card-line my-3 border border-1"></div>
+                {{-- card bottom --}}
+                <div class="card-bottom">
+                    <div class="price mt-3 text-end">
+                        <span>&euro;{{$train->ticket_price}}</span>
+                    </div>
+
                 </div>
             </div>
         </div>
