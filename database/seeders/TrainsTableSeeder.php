81b70ec778292ef26893f46d585e1e7695293cb7
marraguglielmo <gugmarra@gmail.com>
2024-05-14 16:56:53 +0200

    [add] seeder

--- database/seeders/TrainsTableSeeder.php
+++ database/seeders/TrainsTableSeeder.php
@@ -14,21 +14,69 @@ class TrainsTableSeeder extends Seeder
     public function run(): void
     {
 
-        dump('Prova seeder ☻');
         $data = [
             [
                 'company' => 'Frecciarossa',
+                'slug' => 'Frecciarossa-FR152',
                 'conductor' => 'Giuseppe Rossi',
                 'departure_station' => 'Lecce',
                 'arrival_station' => 'Brindisi',
-                'departure_time' => '2024-05-14 09:30:00',
-                'arrival_time' => '2024-05-14 11:10:00',
+                'departure_time' => '09:30:00',
+                'arrival_time' => '11:10:00',
                 'ticket_price' => 15.00,
                 'code' => 'FR152',
                 'coaches_number' => '10',
                 'is_on_time' => true,
                 'is_cancelled' => false
-            ]
+            ],
+            [
+                'company' => 'Frecciargento',
+                'slug' => 'Frecciargento-FA412',
+                'conductor' => 'Pinco Pallino',
+                'departure_station' => 'Milano Centrale',
+                'arrival_station' => 'Roma Termini',
+                'departure_time' => '08:00:00',
+                'arrival_time' => '10:40:00',
+                'ticket_price' => 55.00,
+                'code' => 'FA412',
+                'coaches_number' => '14',
+                'is_on_time' => false,
+                'is_cancelled' => false
+            ],
+            [
+                'company' => 'Eurostar',
+                'slug' => 'Eurostar-EU860',
+                'conductor' => 'Giorgio Ponzo',
+                'departure_station' => 'Napoli Centrale',
+                'arrival_station' => 'Palermo Centrale',
+                'departure_time' => '11:30:00',
+                'arrival_time' => '15:45:00',
+                'ticket_price' => 43.50,
+                'code' => 'EU860',
+                'coaches_number' => '9',
+                'is_on_time' => false,
+                'is_cancelled' => true
+            ],
         ];
+
+
+        foreach($data as $item){
+            $new_train = new Train();
+            $new_train->company = $item['company'];
+            $new_train->slug = $item['slug'];
+            $new_train->conductor = $item['conductor'];
+            $new_train->departure_station = $item['departure_station'];
+            $new_train->arrival_station = $item['arrival_station'];
+            $new_train->departure_time = $item['departure_time'];
+            $new_train->arrival_time = $item['arrival_time'];
+            $new_train->ticket_price = $item['arrival_time'];
+            $new_train->code = $item['ticket_price'];
+            $new_train->coaches_number = $item['code'];
+            $new_train->is_on_time = $item['coaches_number'];
+            $new_train->is_cancelled = $item['is_on_time'];
+
+            dump($new_train);
+        }
+
     }
 }
